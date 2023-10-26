aa08ec62057900dc2663a66724645bf17e27620c
Lysenko <lysenkoden@gmail.com>
2023-10-26 22:14:02 +0200

    my code to delete book from list

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deleteBook } from "../../redux/books/actionCreators";
 import "./BookList.css";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -15,6 +19,11 @@ const BookList = () => {
               <li key={book.id}>
                 <div className="book-nfo">
                   {++index}. {book.title} by <strong>{book.author}</strong>
+                </div>
+                <div className="book-actions">
+                  <button onClick={() => dispatch(deleteBook(book.id))}>
+                    Delete
+                  </button>
                 </div>
               </li>
             );
--- frontend/src/redux/books/actionCreators.js
+++ frontend/src/redux/books/actionCreators.js
@@ -3,3 +3,7 @@ import * as a from "./actionTypes";
 export const addBook = (newBook) => {
   return { type: a.ADD_BOOK, payload: newBook };
 };
+
+export const deleteBook = (idBook) => {
+  return { type: a.DELETE_BOOK, payload: idBook };
+};
