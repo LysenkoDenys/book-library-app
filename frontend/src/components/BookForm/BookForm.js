a2ae0876353950cef1a3a14b650ce89ad4b6a1d5
Lysenko <lysenkoden@gmail.com>
2023-10-27 21:30:00 +0200

    separate function for book creation

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
 import { addBook } from "../../redux/books/actionCreators";
+import createBookWithID from "../../utils/createBookWithID";
 import booksData from "../../data/books.json";
 import "./BookForm.css";
 
@@ -13,11 +13,8 @@ const BookForm = () => {
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    };
+
+    const randomBookWithId = createBookWithID(randomBook);
     dispatch(addBook(randomBookWithId));
   };
 
@@ -25,13 +22,7 @@ const BookForm = () => {
     event.preventDefault();
     // dispatch action
     if (title && author) {
-      const book = {
-        title: title,
-        author: author,
-        id: uuidv4(),
-        isFavorite: false,
-      };
-      dispatch(addBook(book));
+      dispatch(addBook(createBookWithID({ title: title, author: author })));
       setTitle("");
       setAuthor("");
     }
